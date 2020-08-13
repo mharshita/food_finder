9ab1c48cc8b75ce0e3e5a9036f8f6abc56c74375
mharshita <manglaharshita@gmail.com>
2020-08-14 02:11:03 +0530

    completed frontend for profile page

--- profile.php
+++ profile.php
@@ -20,13 +20,13 @@ include("connection.php");
         <p class="side-tag">delivering the best food at your doorstep :)</p>
 </br></br>
         <h1>
- <span class="text_1">Hungry?</span>
+ <span class="text_1">Hungry??</span>
 <span class="text_2">Order food now!!</span>
 </h1>
 </br></br>
-<a href="home.php" class="button button-accent button-small">Order Now</a>
+<a href="home.php" class="button button-accent button-small"><strong>Order Now</strong></a>
 </br></br>
-<a href="home.php" class="button button-accent button-small">Rate & Review</a> </br>
+<a href="" class="button button-accent button-small"><strong>Rate & Review</strong></a> </br>
 
     </div>
     <div class="column main-page">
@@ -39,15 +39,41 @@ include("connection.php");
            
                 <li><a href="logout.php" class="nav-li">LOGOUT</a></li>
                 <li><a href="" class="nav-li">CART</a></li>
+                <li><a href="home.php" class="nav-li">HOME</a></li>
               
             </ul>
             
         </nav>
 
 <!--</header>-->
+<?php
+    $sql = ("SELECT * FROM user WHERE username = '$loggedUsername' ");
+    $result = mysqli_query($con, $sql);
+    $num = mysqli_num_rows($result);
 
-        <h1 class="main-head">To make changes, just click the edit button!!</h1>
-        <p>Username : <?php echo $loggedUsername ?></p>
+    for($i = 1; $i <= $num; $i++)
+    {
+        $row = mysqli_fetch_array($result);
+?>
+
+        <h1 class="main-head">Profile Page</h1>
+        <table>
+        <tr>
+            <td>Username</td>
+            <td><?php echo $row['username']; ?></td>
+        </tr>
+        <tr>
+            <td>E-mail</td>
+            <td><?php echo $row['e_mail']; ?></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><?php echo $row['password']; } ?></td>
+        </tr>
+        </table>
+        </br></br>
+        <a href="" class="button button-accent button-small"><strong>Edit Details</strong></a>
+        <a href="" class="button button-accent button-small"><strong>Show Previous Reviews</strong></a>
     </div>
 </div>
 
