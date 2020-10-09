75382a6837876073c29caa47471b622f17b64658
mharshita <manglaharshita@gmail.com>
2020-10-09 13:22:03 +0530

    edit option added

--- profile.php
+++ profile.php
@@ -2,7 +2,16 @@
 <?php
 include("connection.php");
     $loggedUsername = $_SESSION['username'];
-?>
+
+    if(isset($_POST['edit']))
+    {
+        $e_mail = $_POST['e_mail'];
+        $password = $_POST['password'];
+
+        $sql = "UPDATE user SET e_mail = '$e_mail', password = '$password' ";
+        $result = mysqli_query($con, $sql);
+    }
+?> 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -57,25 +66,53 @@ include("connection.php");
 ?>
 
         <h1 class="main-head">Profile Page</h1>
-        <table>
+        <table class="profile-main">
         <tr>
-            <td>Username</td>
-            <td><?php echo $row['username']; ?></td>
+            <td class="profile-td">Username</td>
+            <td class="profile-td"><?php echo $row['username']; ?></td>
         </tr>
         <tr>
-            <td>E-mail</td>
-            <td><?php echo $row['e_mail']; ?></td>
+            <td class="profile-td">E-mail</td>
+            <td class="profile-td"><?php echo $row['e_mail']; ?></td>
         </tr>
         <tr>
-            <td>Password</td>
-            <td><?php echo $row['password']; } ?></td>
+            <td class="profile-td">Password</td>
+            <td class="profile-td"><?php echo $row['password']; } ?></td>
         </tr>
         </table>
         </br></br>
-        <a href="" class="button button-accent button-small"><strong>Edit Details</strong></a>
+        <a href="#edit" class="button button-accent button-small"><strong>Edit Details</strong></a>
         <a href="" class="button button-accent button-small"><strong>Show Previous Reviews</strong></a>
     </div>
 </div>
+
+<div id="edit" class="overlay">
+            <div class="popup">
+                <h2>Enter New Details</h2>
+                <a class="close" href="#">&times;</a>
+                <div class="content">
+                    
+                    <form action="" method="POST">
+                        <table>
+                            <tr class="popup-tr">
+                                <td class="popup-td">New E-mail</td>
+                                <td class="popup-td"><input type="text" name="e_mail" id="input"></td>
+                            </tr>
+                            <tr class="popup-tr">
+                                <td class="popup-td">New password</td>
+                                <td class="popup-td"><input type="password" name="password" id="input"></td>
+                            </tr>
+                            <tr class="popup-tr">
+                                <td></td>
+                                <td class="popup-td" style="float: right;"><input type="submit" name="edit" value="EDIT" class="button button-accent button-small"></td>
+                            </tr>
+                        </table>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
 
 </body>
 </html>
